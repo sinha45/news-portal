392f5cf426e738b36d4bc34513446f861aeafab9
sinha45 <alfa45leonidas@gmail.com>
2022-10-18 22:31:02 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -12,23 +12,27 @@ const displayCategories = categories => {
 
     categories.forEach(category => {
         const categoryDiv = document.createElement('div');
+
         categoryDiv.classList.add('cat-btn')
         categoryDiv.innerHTML = `
             <button onclick="loadNews(${category.category_id})" id=${category.category_id} type="button" class="m-4 p-3 border-0 bg-transparent"><span class="text-secondary fw-semibold">${category.category_name}</span></button>
             
+            
         `;
 
         // start loader
-        toggleSpinner(true);
+
 
 
         categoryContainer.appendChild(categoryDiv);
+        toggleSpinner(true);
     });
 
     // stop loader
     toggleSpinner(false);
 
 };
+
 // start loader
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
@@ -45,13 +49,14 @@ const loadNews = async () => {
 
     const res = await fetch(url)
     const data = await res.json()
+    console.log(data.data);
     displayNews(data.data)
     // console.log(newsUrl);
 }
 
 const displayNews = news => {
     const newsContainer = document.getElementById('news-container');
-    newsContainer.innerText = '';
+
     news.forEach(singleNews => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
@@ -70,7 +75,9 @@ const displayNews = news => {
                 <h5 class="mx-3 mt-4">${singleNews.author.name}</h5>
               <h5 class="mx-5 mt-4 m">view: ${singleNews.total_view}</h5>
               <div class="mt-4">
-              <button class="text-light bg-primary mx-5  rounded border-0">Read more</button>
+              <button onclick="loadNewsDetails('${singleNews.details}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal">
+              Read more
+             </button>
               </div>
               </div>
               
@@ -86,7 +93,16 @@ const displayNews = news => {
 
 
     })
+
 }
+
+const loadNewsDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data);
+}
+
 loadNews();
 
 loadCategories();
